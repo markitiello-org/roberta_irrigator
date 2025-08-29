8251997e36b76acf0fdbbb115264ac2e8192e21b
Marco Chimenti <marco.chimenti@stellantis.com>
2025-08-29 23:26:39 +0200

    update zone dao tests

--- backend/dao/test/zone_dao_test.py
+++ backend/dao/test/zone_dao_test.py
@@ -1,43 +1,67 @@
+"""
+Unit tests for ZoneDAO: verifies adding, editing,
+and retrieving zone records and their irrigation info.
+"""
+
 import datetime
 import unittest
 from backend.db.SqlLite import SqlLite
 from backend.datatype.Zone import Zone
 from backend.datatype.irrigation_info import IrrigationInfo
-from backend.dao.ZoneDAO import ZoneDAO
+from backend.dao.zone_dao import ZoneDAO
 
 
 class TestZoneDAO(unittest.TestCase):
+    """
+    Test suite for ZoneDAO database operations.
+    """
+
     db = None
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
+        """
+        Set up the test database before running tests.
+        """
         print("Setting up the database for IrrigationInfoDAO tests")
-        self.db = SqlLite.get_instance()
-        self.db.CreateDb()
+        cls.db = SqlLite.get_instance()
+        cls.db.CreateDb()
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
+        """
+        Remove the test database after running tests.
+        """
         print("Tearing down the database after IrrigationInfoDAO tests")
-        self.db.RemoveDb()
+        cls.db.RemoveDb()
 
     def test_add_zone(self):
+        """
+        Test adding a zone and retrieving it from the database.
+        """
         print("==== test add zone =====")
         zone_to_add = Zone("test", 1)
-        ZoneDAO.AddNewIrrigator(zone_to_add)
-        irrigator = ZoneDAO.GetZoneById(zone_to_add.id)
+        ZoneDAO.add_new_irrigator(zone_to_add)
+        irrigator = ZoneDAO.get_zone_by_id(zone_to_add.id)
         self.assertEqual(irrigator, zone_to_add)
 
     def test_add_zone_with_irrigation_info(self):
+        """
+        Test adding a zone with irrigation info and verifying retrieval.
+        """
         print("==== test add zone =====")
         zone_to_add = Zone("test", 2)
         irrigation_info_to_add = IrrigationInfo(datetime.time(10, 20, 00, 00), 120)
         zone_to_add.irrigation_info.append(irrigation_info_to_add)
-        ZoneDAO.AddNewIrrigator(zone_to_add)
-        zone = ZoneDAO.GetZoneById(zone_to_add.id)
+        ZoneDAO.add_new_irrigator(zone_to_add)
+        zone = ZoneDAO.get_zone_by_id(zone_to_add.id)
         self.assertNotEqual(len(zone.irrigation_info), 0)
         self.assertEqual(zone, zone_to_add)
 
     def test_add_zone_with_more_than_one_irrigation_info(self):
+        """
+        Test adding a zone with multiple irrigation info entries and verifying retrieval.
+        """
         print("==== test add zone with multiple irrigation info =====")
         zone_to_add = Zone("test3", 3)
         irrigation_info_to_add_1 = IrrigationInfo(datetime.time(10, 20, 00, 00), 120)
@@ -46,26 +70,29 @@ class TestZoneDAO(unittest.TestCase):
         zone_to_add.irrigation_info.append(irrigation_info_to_add_1)
         zone_to_add.irrigation_info.append(irrigation_info_to_add_2)
         zone_to_add.irrigation_info.append(irrigation_info_to_add_3)
-        ZoneDAO.AddNewIrrigator(zone_to_add)
-        irrigator = ZoneDAO.GetZoneById(zone_to_add.id)
+        ZoneDAO.add_new_irrigator(zone_to_add)
+        irrigator = ZoneDAO.get_zone_by_id(zone_to_add.id)
         self.assertEqual(len(irrigator.irrigation_info), 3)
         self.assertEqual(irrigator.irrigation_info[0], irrigation_info_to_add_1)
         self.assertEqual(irrigator.irrigation_info[1], irrigation_info_to_add_2)
         self.assertEqual(irrigator.irrigation_info[2], irrigation_info_to_add_3)
 
     def test_edit_zone(self):
+        """
+        Test editing a zone and its irrigation info, then verifying the updates.
+        """
         print("==== test edit zone =====")
         zone_to_add = Zone("test4", 4)
         irrigation_info_to_add = IrrigationInfo(datetime.time(10, 20, 00, 00), 120)
         zone_to_add.irrigation_info.append(irrigation_info_to_add)
-        ZoneDAO.AddNewIrrigator(zone_to_add)
+        ZoneDAO.add_new_irrigator(zone_to_add)
         zone_to_add.name = "updated_test"
         zone_to_add.gpio_pin = 5
         zone_to_add.irrigation_info[0].for_how_many_seconds = 150
         zone_to_add.irrigation_info[0].time_to_start = datetime.time(11, 30, 00, 00)
         zone_to_add.last_irrigation_date = datetime.datetime.now()
-        ZoneDAO.AddNewIrrigator(zone_to_add)
-        irrigator = ZoneDAO.GetZoneById(zone_to_add.id)
+        ZoneDAO.add_new_irrigator(zone_to_add)
+        irrigator = ZoneDAO.get_zone_by_id(zone_to_add.id)
         self.assertEqual(irrigator.name, "updated_test")
         self.assertEqual(irrigator.gpio_pin, 5)
         self.assertEqual(len(irrigator.irrigation_info), 1)
