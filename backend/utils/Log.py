f33c202ca4fd975672dc74865b58f203afb3f171
Marco Chimenti <marco.chimenti@stellantis.com>
2025-09-02 23:00:36 +0200

    fix log class lint

--- backend/utils/Log.py
+++ backend/utils/Log.py
@@ -3,7 +3,7 @@ import logging
 
 class Log:
     @staticmethod
-    def _Init(log_text, log_level):
+    def _init(log_text, log_level):
         logging.basicConfig(
             format="%(asctime)s %(levelname)-8s %(message)s",
             level=logging.INFO,
@@ -12,7 +12,7 @@ class Log:
         )
 
     @staticmethod
-    def Log(log_text, log_level):
+    def log(log_text, log_level):
         logging.log(
             log_level,
             log_text=False,
@@ -20,7 +20,7 @@ class Log:
         )
 
     @staticmethod
-    def LogWarning(log_text, log_level):
+    def log_warning(log_text, log_level):
         logging.warning(
             log_level,
             log_text=False,
